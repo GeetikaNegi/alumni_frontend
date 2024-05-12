83853923ac28574a2e42aa3d5ace32091c78255a
Ankush Kumar Verma <91385833+akvghost@users.noreply.github.com>
2024-05-13 00:08:29 +0530

    Update Home.js

--- alumni_frontend/src/Components/Home.js
+++ alumni_frontend/src/Components/Home.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import ImageSlide from '../Components/imgSlider/ImageSlide';
-import ImgCarousel from '../Components/AlumniCards/ImgCarousel';
+import ImageSlide from './LandingPage/imgSlider/ImageSlide';
+import ImgCarousel from './LandingPage/AlumniCards/ImgCarousel';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import AlumniVision from './AlumniVision/AlumniVision';
 
 
 function Home() {
@@ -11,7 +9,6 @@ function Home() {
     <div>
       <ImageSlide/>
       <ImgCarousel/>
-      <AlumniVision />
     </div>
   )
 }
